1fe513650ac37809920de3b2935c0cd1615e0714
Gurwinder Singh <guri60343@gmail.com>
2021-03-25 21:11:37 +0530

    logic added

--- public/js/multi.js
+++ public/js/multi.js
@@ -1,15 +1,65 @@
 const socket = io.connect('http://localhost:4000');
-var pera = document.getElementById('pera').innerText.split(' ');
-var myText = document.getElementById('myText');
-var myText1;
-var i=0;
-// console.log(pera);
 
-myText.addEventListener('onInput',()=>{
-    myText1 = myText.innerText;
-    console.log(i);
-    if(myText1 == pera[i]){
-        console.log('true');
-        myText.innerText= "";
+const link = "http://api.quotable.io/random";
+
+var pera = document.getElementById('pera');
+var myText = document.getElementById('myText');
+
+function getRandomQuote(){
+    return (fetch(link)
+    .then(response => response.json())
+    .then(myData => myData.content));
+}
+
+//inserting elements from api to pera
+async function getNextQuote(){
+    const quote = await getRandomQuote();
+    // console.log(quote);
+    // pera.innerText = '';
+    quote.split('').forEach(i => {
+        var characterSpan = document.createElement('span'); //this element will created every time newly
+        characterSpan.innerText = i;
+        characterSpan.classList.add('mySpan');
+        pera.appendChild(characterSpan);
+        // console.log(characterSpan);
+        
+    });
+    myText.value = "";  //not neccesary
+}
+
+
+// adding event listener on textarea when stating input
+
+myText.addEventListener('input',()=>{
+    const quoteArray = document.getElementsByClassName('mySpan');
+    // console.log(quoteArray);
+    const myTextArray = myText.value.split('');
+    // console.log(myTextArray);
+    // myTextArray.forEach((element )=> {
+        
+    //     console.log(element);
+    // });
+    for(let i in quoteArray ){
+        if(myTextArray[i] == null){
+            quoteArray[i].classList.remove('correct');
+            quoteArray[i].classList.remove('incorrect');
+        }
+        else if(myTextArray[i] === quoteArray[i].innerText){
+            quoteArray[i].classList.add('correct');
+            quoteArray[i].classList.remove('incorrect');
+        }
+        else{
+            quoteArray[i].classList.remove('correct');
+            quoteArray[i].classList.add('incorrect');
+        }
     }
 })
+
+
+
+
+
+getNextQuote();
+
+// characterSpan.classList.add('incorrect');
+
