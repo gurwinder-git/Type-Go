7533e9e818b2ca1ef041dbfdf2dca29704ca4dd7
Gurwinder Singh <guri60343@gmail.com>
2021-03-27 19:57:12 +0530

    fixes some bugs

--- public/js/multi.js
+++ public/js/multi.js
@@ -4,15 +4,61 @@ const link = "http://api.quotable.io/random";
 
 var pera = document.getElementById('pera');
 var myText = document.getElementById('myText');
+var startBtn = document.getElementById('startBtn');
+var timeLeftSpan = document.getElementById('timeLeftSpan');
+var textInSmallTag = document.getElementById('textInSmallTag');
+var timeLeft = 6;
 
-function getRandomQuote(){
+startBtn.addEventListener('click', () => {
+    console.log('clicked');
+    startBtn.disabled = true;
+    countDown();
+    setTimeout(getNextQuote,2000);
+});
+
+
+function countDown() {
+    setTimeout(() => {
+        timeLeft--;
+        
+        if (timeLeft >= 0) {
+            textInSmallTag.innerText = "starting in: ";
+            timeLeftSpan.innerText = timeLeft;
+            switch (timeLeft) {
+                case 1:
+                    myText.disabled = false;
+                    myText.focus();
+                case 0:
+                    timeLeftSpan.innerText = "";
+                    console.log('im here');
+                    textInSmallTag.innerText = 'Start Typing...';
+                    break;
+                case 3:
+                    textInSmallTag.classList.add('red');
+                    textInSmallTag.classList.remove('green');
+                    timeLeftSpan.classList.add('red');
+                    timeLeftSpan.classList.remove('green');
+                    break;
+                default:
+            }
+            // timeLeftSpan.innerText = timeLeft;
+            countDown();
+        }
+    }, 1000)
+}
+
+
+
+
+
+function getRandomQuote() {
     return (fetch(link)
-    .then(response => response.json())
-    .then(myData => myData.content));
+        .then(response => response.json())
+        .then(myData => myData.content));
 }
 
 //inserting elements from api to pera
-async function getNextQuote(){
+async function getNextQuote() {
     const quote = await getRandomQuote();
     // console.log(quote);
     // pera.innerText = '';
@@ -22,7 +68,7 @@ async function getNextQuote(){
         characterSpan.classList.add('mySpan');
         pera.appendChild(characterSpan);
         // console.log(characterSpan);
-        
+
     });
     myText.value = "";  //not neccesary
 }
@@ -30,36 +76,27 @@ async function getNextQuote(){
 
 // adding event listener on textarea when stating input
 
-myText.addEventListener('input',()=>{
+myText.addEventListener('input', () => {
     const quoteArray = document.getElementsByClassName('mySpan');
     // console.log(quoteArray);
     const myTextArray = myText.value.split('');
     // console.log(myTextArray);
     // myTextArray.forEach((element )=> {
-        
+
     //     console.log(element);
     // });
-    for(let i in quoteArray ){
-        if(myTextArray[i] == null){
+    for (let i in quoteArray) {
+        if (myTextArray[i] == null) {
             quoteArray[i].classList.remove('correct');
             quoteArray[i].classList.remove('incorrect');
         }
-        else if(myTextArray[i] === quoteArray[i].innerText){
+        else if (myTextArray[i] === quoteArray[i].innerText) {
             quoteArray[i].classList.add('correct');
             quoteArray[i].classList.remove('incorrect');
         }
-        else{
+        else {
             quoteArray[i].classList.remove('correct');
             quoteArray[i].classList.add('incorrect');
         }
     }
 })
-
-
-
-
-
-getNextQuote();
-
-// characterSpan.classList.add('incorrect');
-
