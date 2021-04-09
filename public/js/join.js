bb6923ffba22e0756d957cfd4970e9b24f1f7fef
Gurwinder Singh <guri60343@gmail.com>
2021-04-09 18:09:48 +0530

    silly mistake

--- public/js/join.js
+++ public/js/join.js
@@ -1,5 +1,5 @@
-var socket = io.connect('http://localhost:4000');
-// var socket = io.connect('https://type-go-multiplayer.herokuapp.com/');
+// var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://type-go-multiplayer.herokuapp.com/');
 
 const link = "https://api.quotable.io/random";
 
@@ -28,7 +28,7 @@ socket.emit('joinRoom',{
 
 socket.on('joinError',(message)=>{
     localStorage.setItem('joinError', JSON.stringify(message));
-    window.location.href = "http://localhost:4000";
+    window.location.href = 'https://type-go-multiplayer.herokuapp.com/';
 })
 
 //listerm startGame event
@@ -270,6 +270,6 @@ socket.on('Adminleft', (idOfUser)=>{
 
     if(getElement.classList.contains(hasClassName)){
         localStorage.setItem('adminLeftGame',JSON.stringify("Adimn left the Game"))
-        window.location.href = "http://localhost:4000";
+        window.location.href = 'https://type-go-multiplayer.herokuapp.com/';
     }
 })
